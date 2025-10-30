69329ffecce4ee8535c2674aa27d74fa332a0f55
kmn9 <148010373+kmn9@users.noreply.github.com>
2025-10-30 20:47:23 +0100

    Add files via upload

---
+++ site-lib/scripts/back-button.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const container = document.createElement("div");
+  container.className = "back-container";
+  container.innerHTML = `
+    <a href="../index.html" class="back-button">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15 18l-6-6 6-6" />
+      </svg>
+      Back to main page
+    </a>
+  `;
+  document.body.insertBefore(container, document.body.firstChild);
+});
