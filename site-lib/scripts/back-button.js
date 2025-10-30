dee27478c5bc39a508ae504db66040f1953cd9e9
kmn9 <148010373+kmn9@users.noreply.github.com>
2025-10-30 20:52:00 +0100

    Update back button link to point to GitHub Pages

--- site-lib/scripts/back-button.js
+++ site-lib/scripts/back-button.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   const container = document.createElement("div");
   container.className = "back-container";
   container.innerHTML = `
-    <a href="../index.html" class="back-button">
+    <a href="https://kmn9.github.io/theopendoor/index.html" class="back-button">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" d="M15 18l-6-6 6-6" />
       </svg>
@@ -10,4 +10,5 @@ document.addEventListener("DOMContentLoaded", function() {
     </a>
   `;
   document.body.insertBefore(container, document.body.firstChild);
+
 });
